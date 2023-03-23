4d3907b0fb5f17ad8b1a4f3d4afba220693024c4
abdullahdurrani <m.abdullahdurrani7@gmail.com>
2023-03-23 22:03:44 +0500

    data ingested from api to raw in minio bucket

--- scripts/data_ingestion.py
+++ scripts/data_ingestion.py
@@ -1,5 +1,8 @@
-import requests
 from vars import *
+import json
+import boto3
+import requests
+from datetime import date
 
 # Use HTTPBasicAuth to authenticate the request with the Reddit API
 auth = requests.auth.HTTPBasicAuth(personal_use_script, secret_token)
@@ -23,7 +26,25 @@ TOKEN = res.json()['access_token']
 headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
 
 # Send a GET request to get the hot posts in the "popular" subreddit
-res = requests.get("https://oauth.reddit.com/r/popular/hot",
+res = requests.get("https://oauth.reddit.com/r/popular",
                    headers=headers)
 
-print(res.json())
+# Get the JSON data from the response
+data = res.json()
+
+# Convert the JSON data to a string
+json_data = json.dumps(data)
+
+# Create a boto3 client for MinIO
+s3_client = boto3.client('s3',
+                         endpoint_url=minio_endpoint,
+                         aws_access_key_id=minio_access_key,
+                         aws_secret_access_key=minio_secret_key,
+                         region_name=minio_region)
+
+file_date = date.today().strftime('%Y%m%d')
+
+# Upload the JSON data to the MinIO bucket
+s3_client.put_object(Bucket=minio_bucket,
+                     Key=f'raw/popular_{file_date}.json',
+                     Body=json_data.encode('utf-8'))
